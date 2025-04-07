578d28d2a823a10fd0911e43072cd6f7254283f6
Miya <miya.wujing@gmail.com>
2025-04-07 18:50:30 +1200

    Feat: Learn page

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,16 +1,13 @@
+import LearnImg from '../assets/learn/learn.svg';
+import Footer from '../components/Footer';
+
 const Learn = () => {
   return (
-    <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-7xl mx-auto">
-        <h1 className="text-3xl font-bold text-gray-900 mb-8">Learning Journey</h1>
-        <div className="bg-white shadow overflow-hidden sm:rounded-lg p-6">
-          <div className="space-y-6">
-            <p className="text-gray-600">
-              Here you'll find my educational background and continuous learning journey.
-            </p>
-          </div>
-        </div>
+    <div className="min-h-screen bg-dark py-12 px-4 sm:px-6 lg:px-8 ">
+      <div className='flex flex-col items-center justify-center  mt-40 mb-40'>
+      <img src={LearnImg} alt="Learn" />
       </div>
+      <Footer />
     </div>
   );
 };
