a650ac1baa3893854c25f740272e5a5327b5c877
Miya <miya.wujing@gmail.com>
2025-04-13 23:26:25 +1200

    Refactor: Impove Skill page responsiveness on small screens

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,9 @@
-
 import Language from '../assets/skill/language.svg';
 import Line from '../assets/skill/line.svg';
 import SkillsImg from '../assets/skill/skills.svg';
+import language1 from '../assets/skill/language1.svg';
+import language2 from '../assets/skill/language2.svg';
+
 
 const Skills = () => {
 
@@ -9,11 +11,17 @@ const Skills = () => {
   return (
     <div className="min-h-screen bg-dark py-12 px-4 sm:px-6 lg:px-8">
       <div className='flex flex-col items-center justify-center mt-20 mb-40'>
-        <img src={Language} alt="Language" />
-        <img src={Line} alt="Line" className='mt-40'  />
-        <img src={SkillsImg} alt="Skills" className='mt-40' />
+
+        <img src={Language} alt="Language" className='hidden md:block' />
+        <div className='flex flex-col items-center justify-center -mt-20 md:hidden'>
+          <img src={language1} alt="language1" />
+          <img src={language2} alt="language2" className='mt-4' />
+        </div>
+
+        <img src={Line} alt="Line" className=' mt-20 md:mt-40' />
+        <img src={SkillsImg} alt="Skills" className='mt-16 md:mt-40' />
       </div>
-   
+
     </div>
   );
 };
