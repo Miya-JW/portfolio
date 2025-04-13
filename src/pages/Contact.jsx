c5117c4d86cecac05f6b349f46121b5fec331e52
Miya <miya.wujing@gmail.com>
2025-04-14 00:15:26 +1200

    Refactor: Improve Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,41 @@
-import ContectImg from '../assets/resume/contact.svg';
 import contact1 from '../assets/resume/contact1.svg';
-import contact2 from '../assets/resume/contact2.svg';
+import phone from '../assets/resume/phone.svg';
+import email from '../assets/resume/email.svg';
+import address from '../assets/resume/address.svg';
+import linkedin from '../assets/resume/linkedin.svg';
+import github from '../assets/resume/github.svg';
 const Contact = () => {
   return (
     <div className='min-h-screen flex items-center justify-center '>
-        <img src={ContectImg} alt="Contact" className='-mt-80 hidden md:block' />
-        <div className='flex flex-col items-center justify-center -mt-72 md:hidden'>
-          <img src={contact1} alt="contact1" className='w-1/2' />
-          <img src={contact2} alt="contact2" className='w-1/2 mt-4' />
+   
+
+      <div className='flex flex-col items-center justify-center -mt-72 md:flex-row md:space-x-40'>
+        <img src={contact1} alt="contact1" className='w-1/2' />
+
+        <div className='flex flex-col items-start justify-start  mt-12 space-y-4 text-sm md:w-1/2 md:text-xl'>
+          <div className='flex flex-row items-center justify-start space-x-4'>
+            <img src={phone} alt="phone" className='w-[25px] md:w-[50px]' />
+            <div>+64-027-455-1830</div>
+          </div>
+          <div className='flex flex-row items-center justify-start space-x-4'>
+            <img src={email} alt="email" className='w-[25px] md:w-[50px]' />
+            <div>miya.wujing@gmail.com</div>
+          </div>
+          <div className='flex flex-row items-center justify-start space-x-4'>
+            <img src={address} alt="address" className='w-[25px] md:w-[50px]' />
+            <div>Hamilton, New Zealand</div>
+          </div>
+          <div className='flex flex-row items-center justify-start space-x-4'>
+            <img src={linkedin} alt="linkedin" className='w-[25px] md:w-[50px]' />
+            <div className='hover:text-blue-500 hover:underline'>https://www.linkedin.com/in/miya-j-wu/</div>
+          </div>
+          <div className='flex flex-row items-center justify-start space-x-4'>
+            <img src={github} alt="github" className='w-[25px] md:w-[50px]' />
+            <div className='hover:text-blue-500 hover:underline'>https://github.com/Miya-JW</div>
+          </div>
         </div>
+      </div>
+
     </div>
   );
 };
